9f00b52e75084ad502929f5a16bd554080a24255
Piotr <p.chrzanowski@gmail.com>
2024-07-12 14:51:24 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,68 @@
+const { app, BrowserWindow, Menu } = require('electron')
+const path = require('node:path')
+
+const template = [
+  {
+    label: 'File',
+    submenu: [
+      { role: 'quit' }
+    ]
+  },
+  {
+    label: 'View',
+    submenu: [
+      { 
+        role: 'reload',
+        accelerator: 'F5'
+       },
+      { 
+        role: 'forceReload',
+        accelerator: 'Ctrl+F5'
+       },
+      { 
+        role: 'toggleDevTools', 
+        accelerator: 'F12' 
+      },
+      { type: 'separator' },
+      { role: 'resetZoom' },
+      { role: 'zoomIn' },
+      { role: 'zoomOut' },
+      { type: 'separator' },
+      { 
+        role: 'togglefullscreen',
+        accelerator: 'F11'
+      }
+    ]
+  },
+]
+
+const menu = Menu.buildFromTemplate(template)
+Menu.setApplicationMenu(menu)
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+      width: 800,
+      height: 600,
+      webPreferences: {
+        preload: path.join(__dirname, 'preload.js')
+      }
+    })
+  
+    win.loadFile('index.html')
+  }
+
+  app.whenReady().then(() => {
+    createWindow() //utworzenie okna po aktywowaniu aplikacji
+  
+    app.on('activate', () => {
+      if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow(); //otworzenie okna kiedy wszystkie są zamknięte i aplikacja jest uruchomiona
+      }
+    })
+  })
+  
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+      app.quit() //wyłącza aplikację jak zamknięte są wszystkie okna (nie na Mac)
+    }
+  })
